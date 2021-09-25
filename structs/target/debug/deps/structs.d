ea660f9cfe7143c0c0ef13beef21f9d740cc5e21
danielpark <danielpark1140@gmail.com>
2021-09-25 15:45:32 -0400

    Basic Rust programming, Ownership, Structs

---
+++ structs/target/debug/deps/structs.d
@@ -0,0 +1,5 @@
+/Users/danielpark/Dev/Projects/learning-rust/structs/target/debug/deps/structs: src/main.rs
+
+/Users/danielpark/Dev/Projects/learning-rust/structs/target/debug/deps/structs.d: src/main.rs
+
+src/main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+/Users/danielpark/Dev/Projects/learning-rust/structs/target/debug/structs: /Users/danielpark/Dev/Projects/learning-rust/structs/src/main.rs
